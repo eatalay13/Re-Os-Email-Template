8e6bc9f15ced517c3c0e257c386c0b773aaf7dfd
Emrah Atalay <emrahatalay@gmail.com>
2021-06-25 17:11:05 +0300

    Add project files.

---
+++ Re-Os Email Template/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace Re_Os_Email_Template
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Klasör Adı :";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(87, 9);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(234, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(86, 41);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(234, 21);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Template No :";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(217, 68);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Tümüne Uygula";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(13, 73);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(107, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Sadece Markalar";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(333, 98);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Re-Os Email Template Yönetim";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Re-Os Email Template/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Re_Os_Email_Template
+{
+    public partial class Form1 : Form
+    {
+        private string _selectedFolder;
+        private string _selectedTemplate;
+        private bool _onlyBrand;
+
+        private const string BaseDirectory = "C:/Templates/";
+
+        private List<string> _allFolders;
+        private List<string> _templates;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _allFolders = GetSubDirectories(BaseDirectory);
+
+            comboBox1.DataSource = _allFolders;
+
+            _templates = GetSubDirectories($"{BaseDirectory}{_allFolders.First()}/");
+
+            comboBox2.DataSource = _templates;
+
+            _onlyBrand = checkBox1.Checked;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            _onlyBrand = checkBox1.Checked;
+        }
+
+        private List<string> GetSubDirectories(string path)
+        {
+            string[] subDirectoryEntries = Directory.GetDirectories(path);
+
+            var foldersName = subDirectoryEntries.Select(subDirectory => subDirectory.Substring(path.Length)).ToList();
+
+            return foldersName;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            _selectedFolder = comboBox1.SelectedItem.ToString();
+
+            _templates = GetSubDirectories($"{BaseDirectory}{_selectedFolder}/");
+
+            comboBox2.DataSource = _templates;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _selectedTemplate = comboBox2.SelectedItem.ToString();
+
+            if (string.IsNullOrWhiteSpace(_selectedFolder))
+                _selectedFolder = comboBox1.SelectedItem.ToString();
+
+            if (_onlyBrand)
+                _allFolders = _allFolders.Where(s => s.Contains("Brand")).ToList();
+
+            var fullPath = $"{BaseDirectory}{_selectedFolder}/{_selectedTemplate}/tr.liquid";
+
+            foreach (var folder in _allFolders)
+            {
+                if (!Directory.Exists($"{BaseDirectory}{folder}/{_selectedTemplate}"))
+                    Directory.CreateDirectory($"{BaseDirectory}{folder}/{_selectedTemplate}");
+
+                try
+                {
+                    File.Copy(fullPath, $"{BaseDirectory}{folder}/{_selectedTemplate}/tr.liquid", true);
+                }
+                catch (IOException exception)
+                {
+                    if (exception.Message.Contains("in use"))
+                    {
+                        var process = new Process
+                        {
+                            StartInfo =
+                            {
+                                UseShellExecute = false,
+                                RedirectStandardOutput = true,
+                                FileName = "cmd.exe",
+                                Arguments = "/C copy \"" + fullPath + "\" \"" +
+                                            $"{BaseDirectory}{folder}/{_selectedTemplate}/tr.liquid" + "\""
+                            }
+                        };
+                        process.Start();
+                        Console.WriteLine(process.StandardOutput.ReadToEnd());
+                        process.WaitForExit();
+                        process.Close();
+                    }
+                }
+            }
+
+            MessageBox.Show(@"Seçilen Template Hepsine Uygulandı.", @"Bitti", MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+        }
+    }
+}
